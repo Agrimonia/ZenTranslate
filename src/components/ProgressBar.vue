5f8a10b91367053b52922f1dd39b81c855280bc3
Yongcong Lin <646677472@qq.com>
2017-08-23 16:33:14 +0800

    style: Switch position
    Change the locations of two compoments: editingArea and progressBar.

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="percentage !== 100">
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="percentage"></el-progress>
+  </div>
+  <div v-else> 
+    <el-progress :text-inside="true" :stroke-width="18" :percentage="100" status="success"></el-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    percentage() {
+      return Math.floor(
+        (this.$store.state.currentIndex * 100) / this.$store.state.translatedSentences.length);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
